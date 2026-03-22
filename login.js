7b56c9cc06d0daa256a05554bbd90bc217b2e14a
Your Name <negiaayush2024@gmail.com>
2026-03-22 22:26:49 +0530

    login & sign

--- login.js
+++ login.js
@@ -1,4 +1,44 @@
-document.getElementById("loginBtn").addEventListener("click", () => {
+const { createClient } = supabase
+
+const supabaseUrl = 'https://nwqrllqgiagahnooefgu.supabase.co'
+const supabaseKey = 'sb_publishable_jfUKvzvC7RGHs9gqlVS_lQ_3a4sKHej'
+
+const client = createClient(supabaseUrl, supabaseKey)
+
+
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+    let email = document.getElementById("emailInput").value.trim()
+    let pass = document.getElementById("passInput").value.trim()
+    let error = document.getElementById("errorMsg")
+
+    if (email === "" || pass === "") {
+        error.textContent = "⚠️ Please fill in all fields."
+        error.classList.add("show")
+        return
+    }
+
+    if (pass.length < 6) {
+        error.textContent = "⚠️ Password minimum 6 characters hona chahiye!"
+        error.classList.add("show")
+        return
+    }
+
+    const { data, error: authError } = await client.auth.signInWithPassword({
+        email: email,
+        password: pass
+    })
+
+
+    if (authError) {
+        error.textContent = "❌ " + authError.message
+        error.classList.add("show")
+    } else {
+        window.location.replace("index.html")
+    }
+})
+
+document.getElementById("signupBtn").addEventListener("click", async () => {
     let email = document.getElementById("emailInput").value.trim()
     let pass  = document.getElementById("passInput").value.trim()
     let error = document.getElementById("errorMsg")
@@ -9,11 +49,21 @@ document.getElementById("loginBtn").addEventListener("click", () => {
         return
     }
 
-    window.location.replace("index.html")
-})
+    if (pass.length < 6) {
+        error.textContent = "⚠️ Password minimum 6 characters hona chahiye!"
+        error.classList.add("show")
+        return
+    }
 
-document.addEventListener("keydown", (e) => {
-    if (e.code === "Enter") {
-        document.getElementById("loginBtn").click()
+    const { data, error: authError } = await client.auth.signUp({
+        email: email,
+        password: pass
+    })
+
+    if (authError) {
+        error.textContent = "❌ " + authError.message
+        error.classList.add("show")
+    } else {
+        window.location.replace("index.html")
     }
 })
